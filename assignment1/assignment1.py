89e506cd3f9bc26a0acf6541afef840d612fbd55
Navie Vurdien <nvurdien@gmail.com>
2018-03-16 19:06:46 -0700

    without plots

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import csv
-
 from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 
 def main():
@@ -9,30 +9,22 @@ def main():
     raw_data = open(filename, 'rt')
     reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
     x = list(reader)
-    data_x_train, data_x_test, data_y_train, data_y_test = ([] for i in range(4))
+    x = x[1:]
+    data_x_train, data_x_test, data_y_train, data_y_test = ([] for _ in range(4))
     for train in x[20:]:
         data_y_train.append(float(train[4]))
         data_x_train.append((float(train[0]), float(train[1]), float(train[2]), float(train[3])))
-    for test in x[1:20]:
+    for test in x[:200]:
         data_y_test.append(float(test[4]))
         data_x_test.append((float(test[0]), float(test[1]), float(test[2]), float(test[3])))
     # Create linear regression object
     regr = linear_model.LinearRegression()
     regr.fit(data_x_train, data_y_train)
     data_y_pred = regr.predict(data_x_test)
-    plt.figure(1)
-    for xe, ye in zip(data_x_test, data_y_test):
-        plt.scatter(xe, [ye] * len(xe))
-    plt.plot(data_x_test, data_y_pred)
-    print(data_y_pred)
-    print(data_y_test)
-    plt.yscale('linear')
-    plt.title('TM')
-    plt.grid(True)
-    plt.show()
+    print("Mean Absolute Error", mean_absolute_error(data_y_test, data_y_pred))
+    print("Mean Squared Error", mean_squared_error(data_y_test, data_y_pred))
 
     ####### Best fit ########
-    plt.figure(2)
 
 if __name__ == "__main__":
     main()
