5e0d82f4af65370d72490bde6aece6dbec3e91a5
Navie Vurdien <nvurdien@gmail.com>
2018-03-16 19:53:56 -0700

    finished

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -17,10 +17,9 @@ def main():
         y.append(float(train[4]))
         X.append((float(train[0]), float(train[1]), float(train[2]), float(train[3])))
 
-    data_x_train, data_x_test, data_y_train, data_y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
     ######################## Multiple Linear Regression ########################
 
+    data_x_train, data_x_test, data_y_train, data_y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     regr = linear_model.LinearRegression()
     regr.fit(data_x_train, data_y_train)
     data_y_pred = regr.predict(data_x_test)
@@ -31,25 +30,19 @@ def main():
     ######################## Polynomial fit ########################
 
     # Preprocessing: converting to polynomial matrix
-
-    poly_features = PolynomialFeatures(degree=5)
+    poly_features = PolynomialFeatures(degree=4)
     X_poly = poly_features.fit_transform(X)
 
     # Splitting the dataset into the Training set and Test set
-    X_train, X_test, y_train, y_test = train_test_split(X_poly, y, test_size=0.02, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X_poly, y, test_size=0.2, random_state=0)
 
     # Fitting polynomial regression to the dataset
     from sklearn.linear_model import LinearRegression
-    # poly_features.fit(X_poly, y_train)
     lin_reg = LinearRegression()
     lin_reg.fit(X_train, y_train)
 
     # Model prediction using test set
     y_pred = lin_reg.predict(X_test)
-
-    # plot results
-    # print(y_pred)
-    # print(data_y_test)
     print("\nPolynomial Fit Errors:")
     print("Mean Absolute Error", mean_absolute_error(y_test, y_pred))
     print("Mean Squared Error", mean_squared_error(y_test, y_pred), "\n")
